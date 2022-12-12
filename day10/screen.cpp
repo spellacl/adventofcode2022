1635f045c47d4e36cfadabec04882af80a103453
Louise Spellacy <spellacl@users.noreply.github.com>
2022-12-12 23:09:41 +0000

    Advent of Code 2022: Day 10 Part 2
    Took ages to get back to doing this part

--- day10/screen.cpp
+++ day10/screen.cpp
@@ -5,13 +5,27 @@
 #include <algorithm>
 #include <set>
 
+void part1(int cycle, int x, int &check_cycle, long &signal_strength)
+{
+    if(cycle == check_cycle)
+    {
+        check_cycle += 40;
+        signal_strength += cycle * x;
+    }
+}
+
+void part2(int cycle, int x, std::vector<char>& CRT)
+{
+    int pixel = (cycle - 1)%40;
+    if( pixel == (x-1) || pixel == x || pixel == (x+1) )
+    {
+        CRT.at(cycle - 1) = '#';
+    }
+}
+
 int main(int argc, char * argv[])
 {
     const char * filename = argv[1];
-
-    bool part2 = false;
-    if(argc == 3)
-        part2 = true;
 
     FILE* file = fopen(filename, "r");
     if(file)
@@ -23,43 +37,46 @@ int main(int argc, char * argv[])
 
         int check_cycle = 20;
         long signal_strength = 0;
+        std::vector<char> CRT(240, '.');
 
         while(std::fgets(line, sizeof(line), file))
         {
-            std::cout << line << std::endl;
+            //std::cout << line << std::endl;
             char *intr = strtok(line, " ");
 
             if( std::strcmp(intr, "addx") == 0)
             {
-                if(cycle == check_cycle)
-                {
-                    check_cycle += 40;
-                    std::cout << " -- cycle: " << cycle << " " << x << std::endl;
-                    signal_strength += cycle * x;
-                }
+                part1(cycle, x, check_cycle, signal_strength);
+                part2(cycle, x, CRT);
+                //std::cout << " -- cycle: " << cycle << " " << x << " " << CRT.at(cycle-1) << std::endl;
                 cycle += 1;
 
                 int add = atoi(strtok(nullptr, " "));
-                if(cycle == check_cycle)
-                {
-                    check_cycle += 40;
-                    std::cout << " --- cycle: " << cycle << " " << x << std::endl;
-                    signal_strength += cycle * x;
-                }
+                part1(cycle, x, check_cycle, signal_strength);
+                part2(cycle, x, CRT);
+                //std::cout << " --- cycle: " << cycle << " " << x << " " << CRT.at(cycle-1) << std::endl;
                 cycle += 1;
                 x += add;
             }
             else //noop
             {
-                if(cycle == check_cycle)
-                {
-                    check_cycle += 40;
-                    std::cout << " - cycle: " << cycle << " " << x << std::endl;
-                    signal_strength += cycle * x;
-                }
+                part1(cycle, x, check_cycle, signal_strength);
+                part2(cycle, x, CRT);
+                //std::cout << " - cycle: " << cycle << " " << x << " " << CRT.at(cycle-1) << std::endl;
                 cycle += 1;
             }
         };
+
+        int count = 0;
+        for(int i = 0; i < 6; i++)
+        {
+            for(int j = 0; j < 40; j++)
+            {
+                std::cout << CRT.at(count);
+                count ++;
+            }
+            std::cout << std::endl;
+        }
         std::cout << "Signal Strength: "  << signal_strength << std::endl;
 
         fclose(file);
